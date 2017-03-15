4384e716ef8a5036042492e32ba73316567ad2c7
Zach Williams <hey@zachwill.com>
2017-03-14 21:37:50 -0700

    Add a quick ARM example

---
+++ Applied Regression Modeling (Gelman and Hill)/utils.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Combination of Kruschke and 538 colors
+sns.set_palette(["#87ceeb", "#0c62c4", "#f3ae09", "#399f34", "#fd0607"])
+sns.set_context("talk")
